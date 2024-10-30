70291b98d521d2532185a77686ccdcc0f4e8eacf
Dave Townsend <dtownsend@oxymoronical.com>
2024-10-30 09:41:43 +0000

    Fix includes

--- base/react-overrides.js
+++ base/react-overrides.js
@@ -2,5 +2,6 @@ export default {
   rules: {
     "react/require-default-props": "off",
     "react/jsx-props-no-spreading": "off",
-  }
-}
+    "jsx-a11y/label-has-associated-control": "off",
+  },
+};
